f52a4aef985720a4ad8cef84ddac768d7d947832
Thomas Pope <tpope0928@gmail.com>
2019-08-10 19:12:20 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  validates :title, presense: true
+  validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+  validates :content, length: { minimum: 100 }
 end
